efabeb25e3dcd0bae3525e816bf2f03e68d2953c
mvlyforever <123278392@qq.com>
2019-12-31 14:34:37 +0800

    aaaa

--- src/App.vue
+++ src/App.vue
@@ -49,17 +49,17 @@
     },
     methods:{
       initWx(){
-        marketService.get_wxcfg({url:location.href.split('#')[0]}).then(res=>{
+        marketService.get_wxcfg({uri:location.href.split('#')[0]}).then(res=>{
           if (res.code==0){
             console.log('-=-====res.data',res.data)
             let data = res.data
             this.$wx.config({
               debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-              appId: data.appId, // 必填，公众号的唯一标识
+              appId: data.AppId, // 必填，公众号的唯一标识
               timestamp: data.timestamp, // 必填，生成签名的时间戳
-              nonceStr: data.noncestr, // 必填，生成签名的随机串
+              nonceStr: data.nonce_str, // 必填，生成签名的随机串
               signature: data.signature,// 必填，签名
-              jsApiList: ["getLocation"] // 必填，需要使用的JS接口列表
+              jsApiList: ["updateAppMessageShareData","getLocation","updateTimelineShareData"] // 必填，需要使用的JS接口列表
             })
             wx.ready(function () {
               wx.updateAppMessageShareData({
