ed95ae8da4e976bfda5f10ece412210b868b5820
mvlyforever <123278392@qq.com>
2020-01-01 18:17:31 +0800

    aaaa

--- src/components/edit/Edit.vue
+++ src/components/edit/Edit.vue
@@ -170,7 +170,7 @@
                   if (key.indexOf('img')>-1)
                   {
                     if (info[key])
-                      _this.imgs[key]= [{file:'',path:_this.imgBase + info[key]}]
+                      _this.imgs[key]= [{file:'',path:_this.imgBase + '/' + info[key]}]
                   }
                 }
                 console.log('====imgs',_this.imgs)
